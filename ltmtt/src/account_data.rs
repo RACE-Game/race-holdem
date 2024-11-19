fdfdfc7ec1dabcf43682f1f6d91d8e116138dab8
LX <lxedge@sina.com>
2024-11-20 00:11:42 +0800

    feat: add ltmtt lifecycle

---
+++ ltmtt/src/account_data.rs
@@ -0,0 +1,16 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+type Millis = u64;
+
+#[derive(Default, BorshSerialize, BorshDeserialize)]
+pub struct LtMttAccountData {
+    pub entry_start_time: Millis,
+    pub entry_close_time: Millis,
+    pub settle_time: Millis,
+    pub table_size: u8,
+    // start_chips: u64,
+    // blind_info: BlindInfo,
+    // prize_rules: Vec<u8>,
+    // theme: Option<String>, // optional NFT theme
+    // subgame_bundle: String,
+}
