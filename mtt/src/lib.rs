96e42576df0c76b1ceaf68a9dcd694db2b7c89f7
DogLooksGood <doglooksgood@hotmail.com>
2024-11-27 01:35:14 +0800

    Fix sort_ranks

--- mtt/src/lib.rs
+++ mtt/src/lib.rs
@@ -409,9 +409,7 @@ impl Mtt {
 
     fn sort_ranks(&mut self) {
         self.ranks.sort_by(|r1, r2| {
-            r2.chips
-                .cmp(&r1.chips)
-                .then_with(|| r1.position.cmp(&r2.position))
+            r2.chips.cmp(&r1.chips)
         });
     }
 
@@ -1171,4 +1169,30 @@ mod tests {
         assert_eq!(mtt.tables.get(&1).map(|t| t.players.len()), Some(2));
         assert_eq!(mtt.tables.get(&2).map(|t| t.players.len()), Some(2));
     }
+
+
+    // Test sort ranks
+
+    #[test]
+    fn test_sort_ranks_ensures_eliminated_players_keep_order() {
+        let mut mtt = Mtt::default();
+
+        // Active players
+        mtt.ranks.push(PlayerRank::new(1, 5000, PlayerRankStatus::Alive, 0));
+        mtt.ranks.push(PlayerRank::new(2, 10000, PlayerRankStatus::Alive, 1));
+        mtt.ranks.push(PlayerRank::new(3, 7000, PlayerRankStatus::Alive, 2));
+
+        // Eliminated players
+        mtt.ranks.push(PlayerRank::new(4, 0, PlayerRankStatus::Out, 3));
+        mtt.ranks.push(PlayerRank::new(5, 0, PlayerRankStatus::Out, 4));
+
+        mtt.sort_ranks();
+
+        assert_eq!(mtt.ranks[0].id, 2);
+        assert_eq!(mtt.ranks[1].id, 3);
+        assert_eq!(mtt.ranks[2].id, 1);
+        assert_eq!(mtt.ranks[3].id, 4);
+        assert_eq!(mtt.ranks[4].id, 5);
+    }
+
 }
